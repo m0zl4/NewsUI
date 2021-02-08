3201781047d7f07b28f85e2b023a807737bbd3d5
m0zl4 <zayuka040104@gmail.com>
2021-02-08 13:50:50 +0700

    finished project

---
+++ NewsUI/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  NewsUI
+//
+//  Created by muhammad abdul latief on 08/02/21.
+//
+
+import Foundation
+
+struct News : Identifiable {
+    var id = UUID()
+    var title : String
+    var image : String
+    var description : String
+}
